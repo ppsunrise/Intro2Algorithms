2673bb277289082b4c7236ad2adf202485437b70
Jiahuan Pei <364164226@qq.com>
2015-09-21 11:52:07 +0800

    merge-sort

--- Intro2Algorithms.cpp
+++ Intro2Algorithms.cpp
@@ -1,6 +1,5 @@
 // Intro2Algorithms.cpp : 定义控制台应用程序的入口点。
 //
-
 #include "stdafx.h"
 
 //#include <iostream>
@@ -11,8 +10,8 @@ template <class T>
 int getArrayLen(T& array){
 	return (sizeof(array) / sizeof(array[0]));
 }
-//Best:positive sequence, n; Worst:inverted sequence, n^2 ; Avg:n^2
-float* insertion_sort(float A[],int n){
+//直接插入排序	Best:positive sequence, n; Worst:inverted sequence, n^2 ; Avg:n^2
+void insertion_sort(float *A,int n){
 	float key = 0;
 	int i = 0;
 	//cout << n <<endl;
@@ -25,22 +24,53 @@ float* insertion_sort(float A[],int n){
 		}
 		A[i+1] = key;	
 	}
-	return A;//array can't be return, but a pointer can be used
+}
+void Merge(float *A,int p, int q, int r){
+	int n1,n2,i,j,k;
+	n1 = q - p + 1;//第一部分的长度
+	n2 = r - q;//第二部分的长度
+	float *L = new float[n1+1];
+	float *R = new float[n2+1];
+	for (i = 0;i <= n1 - 1; i++) L[i] = A[p + i];
+	for (j = 0;j <= n2 - 1;j++) R[j] = A[q+j+1];//A[q+1+j-1]
+	L[n1] = INFINITY;//两张哨兵牌
+	R[n2] = INFINITY;
+	for (i = 0, j = 0,k = p;k<=r;k++)
+	{
+		if (L[i]<=R[j]){
+			A[k] = L[i];
+			i++;
+		}else{
+			A[k] = R[j];
+			j++;
+		}
+	}
+
+	delete [] L;//delete要和new要配对使用
+	delete [] R;
+}
+//归并排序
+void merge_sort(float *A,int p,int r){//p指向起始点，r指向终止点，q是分解点
+	int q;
+	if (p<r){
+		q = (p+r)/2;
+		merge_sort(A,p,q);
+		merge_sort(A,q+1,r);
+		Merge(A,p,q,r);
+	}
 }
 int main(){
 	
-	float A[] = {1,3,5,2,4,6};
+	float A[] = {1,3,5,2,4,6,1.3,3.5,5.2,2.4,4.6};
 	int  n = getArrayLen(A);
-	float *B = insertion_sort(A,n);
-	//cout << typeid(B).name() << endl;//check var types
+	//insertion_sort(A,n);//直接插入排序
+	merge_sort(A,0,n-1);//归并排序
 	for (int i=0;i<n;i++)
 	{
-		cout << B[i] << endl;
+		cout << A[i] << endl;
 	}
-	
 
 	return 0;
-	
 }
 
 
--- stdafx.h
+++ stdafx.h
@@ -2,7 +2,8 @@
 // 或是经常使用但不常更改的
 // 特定于项目的包含文件
 //
-
+//#define INFINITY std::numeric_limits<float>::infinity()//定义正无穷
+#define INFINITY 200000//定义正无穷
 #pragma once
 
 #include "targetver.h"
@@ -12,5 +13,6 @@
 #include <iostream>
 #include <cstring>	//prototype for strcmp()
 #include <typeinfo> //for checking type of vars
+#include <limits> //for infinity
 
 // TODO: 在此处引用程序需要的其他头文件
